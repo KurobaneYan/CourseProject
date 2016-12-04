cdb7498c2a93a19ec3d2a2289ab57dbfdd193705
Yan Kurobane <yan.kurobane@gmail.com>
2016-12-04 19:50:32 +0300

    init commit

--- CourseProject/Program.cs
+++ CourseProject/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace CourseProject
 {
@@ -10,6 +7,55 @@ namespace CourseProject
     {
         static void Main(string[] args)
         {
+            string menu;
+            TaskManager taskManager = new TaskManager();
+
+            printHelp();
+
+            while(true)
+            {
+                menu = Console.ReadLine();
+                if (menu.StartsWith("q"))
+                {
+                    break;
+                }
+                if (menu.StartsWith("list"))
+                {
+                    Console.Clear();
+                    taskManager.printAllProcesses();
+                }
+                if (menu.Contains("help"))
+                {
+                    printHelp();
+                }
+                if (menu.StartsWith("kill"))
+                {
+                    Console.Clear();
+                    Console.WriteLine(menu.Substring(5));
+                    try
+                    {
+                        Process.GetProcessById(Convert.ToInt32(menu.Substring(5))).Kill();
+                        Console.WriteLine("Process {0} killed", menu.Substring(5));
+                    } catch
+                    {
+                        Console.WriteLine("Error trying to kill process");
+                    }
+                }
+                if (menu.Contains("test"))
+                {
+                    taskManager.test(menu.Substring(5));
+                }
+            }
+        }
+
+        public static void printHelp()
+        {
+            Console.Clear();
+            Console.WriteLine("Help:");
+            Console.WriteLine("list - prints all processes");
+            Console.WriteLine("kill [process id] - kills process by id");
+            Console.WriteLine("help - prints this help");
+            Console.WriteLine("q - quit");
         }
     }
 }
---
+++ CourseProject/TaskManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace CourseProject
+{
+    class TaskManager
+    {
+        public TaskManager()
+        {
+            
+        }
+        public Process[] getAllProcesses()
+        {
+            return Process.GetProcesses();
+        }
+
+        public void printAllProcesses()
+        {
+            int num = 0;
+            foreach (Process instance in getAllProcesses()) {
+                Console.WriteLine("{0} {1} {2}", num, instance.ProcessName, instance.Id);
+                num+=1;
+            }
+        }
+        public void test(string procName)
+        {
+            foreach (Process process in getAllProcesses().Where(x => x.ProcessName == procName))
+            {
+                using (PerformanceCounter pcProcess = new PerformanceCounter("Process", "% Processor Time", process.ProcessName))
+                using (PerformanceCounter memProcess = new PerformanceCounter("Memory", "Available MBytes"))
+                {
+                    pcProcess.NextValue();
+                    Thread.Sleep(100);
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    float cpuUseage = pcProcess.NextValue();
+                    Console.WriteLine("Process: '{0}' CPU Usage: {1}%", process.ProcessName, cpuUseage);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    float memUseage = memProcess.NextValue();
+                    Console.WriteLine("Process: '{0}' RAM Free: {1}MB", process.ProcessName, memUseage);
+                }
+            }
+        } 
+    }
+}
